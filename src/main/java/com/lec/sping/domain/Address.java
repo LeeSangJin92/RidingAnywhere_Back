22eb7e5b1424fd14f29682663d989ef35463b108
LeeSangJin92 <151722889+LeeSangJin92@users.noreply.github.com>
2024-04-01 23:15:09 +0900

    2024.04. 01 - 라이더 지역 DB 샘플 제작중...

---
+++ src/main/java/com/lec/sping/domain/Address.java
@@ -0,0 +1,24 @@
+package com.lec.sping.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Data
+@AllArgsConstructor
+@Entity
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long addressId;
+
+    private String city;
+    private String town;
+
+}
