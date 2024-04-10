93cd008bfb675bf97e8b5f4ed62c74cd48ea10b8
LeeSangJin92 <151722889+LeeSangJin92@users.noreply.github.com>
2024-04-10 22:55:50 +0900

    2024.04. 09 🛠️ 크루 페이지 작업중... ✅ 크루 매니저 DTO 생성 🛠️ 크루 리스트 작업중...

--- src/main/java/com/lec/sping/dto/CrewManagerDto.java
+++ src/main/java/com/lec/sping/dto/CrewManagerDto.java
@@ -1,14 +1,19 @@
 package com.lec.sping.dto;
 
 import com.lec.sping.domain.crew.Crew;
+import com.lec.sping.domain.crew.CrewManager;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class CrewDataDto {
-    private Crew crew;
+public class CrewManagerDto {
+    private Long crewId;
+    private String crewName;
     private String crewMaster;
+    private List<CrewManager> crewList;
 }
