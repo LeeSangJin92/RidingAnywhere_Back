58a8fa33c56cba4506af680947ea851263ee4e6f
LeeSangJin92 <151722889+LeeSangJin92@users.noreply.github.com>
2024-04-25 23:59:50 +0900

    2024.04. 25 🛠️ 크루 페이지 작업중... 🛠️ 크루 페이지 가입 절차 진행중.. 🛠️ 크루 모집 페이지 작업중... 🛠️ 멤버 디테일 컴포넌트 작성중... 🛠️ 크루 조인 컴포넌트 작성중...

--- ra-app/src/RidingAnywhere-client/component/crewmanager/CrewMemberDetail.jsx
+++ ra-app/src/RidingAnywhere-client/component/crewmanager/CrewMemberDetail.jsx
@@ -10,31 +10,38 @@ const CrewMemberDetail = (props) => {
                 <div className='MemberInfoBox'>
                     <div className='MemberInfoTitle'>
                         <h2>닉네임</h2>
-                        
-                        <h2>출석일</h2>
-                        <h2>가입일</h2>
+                        <h2>지역</h2>
+                        <h2>나이</h2>
+                        <h2>성별</h2>
                     </div>
                     <div className='MemberInfoData'>
                         <h2>닉네임테스트</h2>
-                        <h2>10</h2>
-                        <h2>2024.04.02</h2>
+                        <h2>서울 / 관악</h2>
+                        <h2>92. 01</h2>
+                        <h2>여성</h2>
                     </div>
                 </div>            
             </div>
             <div className='Detail_Mid'>
-                <div className='MemberInfoTitle'>
-                    <h2>나이</h2>
-                    <h2>성별</h2>
-                    <h2>지역</h2>
-                    <h2>연락처</h2>
-                    <h2>이메일</h2>
+                <div className='MemberInfoLine'>
+                    <div className='MemberInfLineTitle'>
+                        <h2>이메일</h2>
+                        <h2>출석일</h2>
+                    </div>
+                    <div className='MemberInfoLineData'>
+                        <h2>yomko65@gmail.com</h2>
+                        <h2>10</h2>
+                    </div>
                 </div>
-                <div className='MemberInfoData'>
-                    <h2>92. 01</h2>
-                    <h2>여성</h2>
-                    <h2>서울 / 관악</h2>
-                    <h2>010-5561-****</h2>
-                    <h2>yomko65@gmail.com</h2>
+                <div className='MemberInfoLine'>
+                    <div className='MemberInfoLineTitle'>
+                        <h2>연락처</h2>
+                        <h2>가입일</h2>
+                    </div>
+                    <div className='MemberInfoLineData'>
+                        <h2>010-5561-****</h2>
+                        <h2>2024.04.02</h2>
+                    </div>
                 </div>
             </div>
             <div className='Detail_Bottom'>
