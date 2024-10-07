28ac9990bfda5de2a63d6265136d3f37c20a2923
LeeSangJin92 <151722889+LeeSangJin92@users.noreply.github.com>
2024-10-07 19:36:13 +0900

    2024. 10. 07 시큐리트 cors 추가
    ➕ http.cors 추가
    ⭐ http.cors( ) 사용 시 스프링 시큐리티 6.0 버전 이후 제거될 예정으로 아래와 같이 수정됨
    
     🛠️ http.cors(Customizer.withDefaults());

--- src/main/java/com/lec/sping/config/WebSecurityConfig.java
+++ src/main/java/com/lec/sping/config/WebSecurityConfig.java
@@ -6,12 +6,10 @@ import com.lec.sping.jwt.TokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -34,6 +32,7 @@ public class WebSecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http.httpBasic((basic)-> basic.disable());
         http.csrf((csrf) -> csrf.disable());
+        http.cors(Customizer.withDefaults());
         http.sessionManagement((sessionManagement)->sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         http.exceptionHandling((exceptionHandling)->exceptionHandling
                 .authenticationEntryPoint(jwtAuthenticationEntryPoint)
